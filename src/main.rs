db6f92415d509c0178ca209686940bc352bcf863
prakash296 <krishnakprakash296@gmail.com>
2023-12-29 15:00:07 +1100

    docker build optimised

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::net::TcpListener;
 
 use actix_web;
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use zero2prod::{
     configuration::get_configuration,
     startup::run,
@@ -24,6 +24,9 @@ async fn main() -> std::io::Result<()> {
 
     // database connection
     let connection_string = configuration.database.connection_string();
-    let connection = PgPool::connect_lazy(&connection_string).expect("database connection failed");
+    let connection = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(&connection_string)
+        .expect("database connection failed");
     run(listner, connection)?.await
 }
